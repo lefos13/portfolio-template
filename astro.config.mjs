3a83999179653325fc31ff48f0df5cdb0b6a9167
Lefteris <lefterisevagelinos1996@gmail.com>
2025-10-12 12:32:25 +0300

    feat: update site configuration and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lefos13.github.io',
+	base: '/portfolio-template',
 	integrations: [mdx(), sitemap()],
 });
